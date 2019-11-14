1e7b23733d149b87d441ad7a0c4c2f3bace3aa75
Helena Rasche <hxr@hx42.org>
2019-11-14 13:54:07 +0100

    ugh whatever

--- bin/scale.py
+++ bin/scale.py
@@ -6,36 +6,40 @@ import multiscaletruchet.svg as svg
 import multiscaletruchet.quadtree as quadtree
 
 
-print(svg.header(author="Helena", title="Multiscale Truchet", width=800, height=800))
+def main():
+    print(svg.header(author="Helena", title="Multiscale Truchet", width=800, height=800))
 
 
-def piece(part, scale=None, offset=None):
-    x, y = offset
-    transform = f"translate({x}, {y}) scale({scale})"
-    return f"""<g transform="{transform}">{part}</g>"""
+    def piece(part, scale=None, offset=None):
+        x, y = offset
+        transform = f"translate({x}, {y}) scale({scale})"
+        return f"""<g transform="{transform}">{part}</g>"""
 
 
-q = quadtree.init(size=900)
-# q.random_divide(kids=0)
-q.divide_min_depth(min_depth=3, min_kids=100)
+    q = quadtree.init(size=900)
+    # q.random_divide(kids=0)
+    q.divide_min_depth(min_depth=3, min_kids=100)
 
 
-def invert(piece):
-    return piece.replace("#ffffff", "black").replace("#000000", "white")
+    def invert(piece):
+        return piece.replace("#ffffff", "black").replace("#000000", "white")
 
 
-for i, child in enumerate(sorted(q.all_children(), key=lambda x: x.depth)):
-    p = random.choice(TILES)
-    x = child.x0 * 0.9
-    y = child.y0 * 0.9
+    for i, child in enumerate(sorted(q.all_children(), key=lambda x: x.depth)):
+        p = random.choice(TILES)
+        x = child.x0 * 0.9
+        y = child.y0 * 0.9
 
-    if child.depth % 2 == 0:
-        pi = invert(p)
-    else:
-        pi = p
+        if child.depth % 2 == 0:
+            pi = invert(p)
+        else:
+            pi = p
 
-    sys.stderr.write(f"{i} - {x} {y} {child.width} {child.depth}\n")
-    sys.stdout.write(piece(pi, offset=(x, y), scale=9 * 2 ** (1 + -child.depth)))
+        sys.stderr.write(f"{i} - {x} {y} {child.width} {child.depth}\n")
+        sys.stdout.write(piece(pi, offset=(x, y), scale=9 * 2 ** (1 + -child.depth)))
 
 
-print(svg.footer())
+    print(svg.footer())
+
+if __name__ == '__main__':
+    main()
--- bin/single.py
+++ bin/single.py
@@ -11,21 +11,25 @@ w = M / 12 * 1000
 h = N / 12 * 1000
 
 
-print(svg.header(author="Helena", title="Singlescale Truchet", width=w, height=h))
+def main():
+    print(svg.header(author="Helena", title="Singlescale Truchet", width=w, height=h))
 
 
-def piece(part, offset=None):
-    x, y = offset
-    transform = f"translate({x}, {y})"
-    return f"""<g transform="{transform}">{part}</g>"""
+    def piece(part, offset=None):
+        x, y = offset
+        transform = f"translate({x}, {y})"
+        return f"""<g transform="{transform}">{part}</g>"""
 
 
-o = 90
+    o = 90
 
-for r in range(M):
-    for c in range(N):
-        p = random.choice(TILES)
-        sys.stdout.write(piece(p, offset=(c * o, r * o)))
+    for r in range(M):
+        for c in range(N):
+            p = random.choice(TILES)
+            sys.stdout.write(piece(p, offset=(c * o, r * o)))
 
 
-print(svg.footer())
+    print(svg.footer())
+
+if __name__ == '__main__':
+    main()
